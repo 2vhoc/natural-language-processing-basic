31ca394c689340b718bd7c010cef74048ae697e9
Vũ Văn Học <2vhoc7@gmail.com>
2025-01-25 22:57:50 +0700

    Create lib.py

---
+++ lib.py
@@ -0,0 +1,4 @@
+import tensorflow
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from sklearn.preprocessing import LabelEncoder
